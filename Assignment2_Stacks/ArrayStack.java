a6228007e9da6449c1d397ae3fbf4173e6b5d60b
Mathew Varughese <mat.varughese3@gmail.com>
2017-09-21 14:52:36 -0400

    completed assignment

--- Assignment2_Stacks/ArrayStack.java
+++ Assignment2_Stacks/ArrayStack.java
@@ -1,25 +1,41 @@
 public class ArrayStack<T> implements Stack<T>{
+	private T[] arr;
+	private int size;
+	
+	public ArrayStack() {
+		arr = (T[])new Object[10];
+		size = 0;
+	}
+	
+	private void ensureCapacity() {
+		if(size == arr.length) {
+			T[] temp = (T[])new Object[arr.length * 2 + 1];
+			System.arraycopy(arr, 0, temp, 0, arr.length);
+			arr = temp;		
+		}
+	}
 
+	private void checkEmptiness() {
+		if(isEmpty()) throw new UnsupportedOperationException("Stack is empty"); 
+	}
+	
 	public T pop() {
-		// TODO Auto-generated method stub
-		return null;
+		checkEmptiness();
+		return arr[--size];
 	}
 
 	public T peek() {
-		// TODO Auto-generated method stub
-		return null;
+		checkEmptiness();
+		return arr[size-1];
 	}
 
-	public void push(Object thing) {
-		// TODO Auto-generated method stub
-		
+	public void push(T thing) {
+		ensureCapacity();
+		arr[size] = thing;
+		size++;
 	}
-
+	
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return size == 0;
 	}
-
-	
-	
 }
---
+++ Assignment2_Stacks/LinkedStack.java
@@ -0,0 +1,68 @@
+public class LinkedStack<T> implements Stack<T> {
+	@SuppressWarnings({ "unused", "hiding" })
+	private class Node<T> {
+		private T value;
+		private Node<T> next;
+		
+		public Node() {}
+		
+		public Node(T _value, Node<T> _next) {
+			value = _value;
+			next = _next;
+		}
+		
+		public Node(T _value) {
+			this(_value, null);
+		}
+		
+		public Node<T> getNext() {
+			return next;
+		}
+		
+		public T getValue() {
+			return value;
+		}
+		
+		public void setNext(Node<T> n) {
+			next = n;
+		}
+	}
+	
+	private Node<T> head;
+	private int size;
+	
+	private void checkEmptiness() {
+		if(isEmpty()) throw new UnsupportedOperationException("Stack is empty"); 
+	}
+	
+	public LinkedStack() {
+		head = null;
+		size = 0;
+	}
+
+	public T pop() {
+		checkEmptiness();
+		T result = head.getValue();
+		head = head.getNext();
+		size--;
+		return result;
+	}
+
+	public T peek() {
+		checkEmptiness();
+		return head.getValue();
+	}
+
+	public void push(T thing) {
+		if(isEmpty()) {
+			head = new Node<T>(thing);
+		} else {
+			head = new Node<T>(thing, head);
+		}
+		size++;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+}
